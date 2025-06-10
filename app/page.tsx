0a40909d9465ef3deeb98a8e4bf4cf65a5acf3d4
Kartik <kartik3344@gmail.com>
2025-06-10 17:57:54 +0530

    theme config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Home page
+    <div className="flex flex-col items-center">
+      <h1>Home page</h1>
+      <Button variant={"outline"}>Hello button</Button>
     </div>
   );
 }
