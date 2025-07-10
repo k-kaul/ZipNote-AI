a86a2732714c7f750dde5241769ca7c599556177
Kartik <kartik3344@gmail.com>
2025-07-10 14:36:10 +0530

    fixed routing

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -30,7 +30,7 @@ export default function HeroSection(){
             <MotionH2 variants={itemVariants} className="text-lg sm:text-xl lg:text-2xl text-center px-4 lg:px-0 lg:max-w-4xl text-gray-600">Get a summary reel of the documents in seconds</MotionH2>
             <MotionDiv variants={itemVariants} whileHover={buttonVariants}>
                 <Button variant={'link'} className=" text-white mt-6 text-base sm:text-lg lg:text-xl rounded-full px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-8 lg:mt-16 bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:no-underline font-bold shadow-lg transition-all duration-300">
-                    <Link href="/#pricing" className="flex gap-2 items-center">
+                    <Link href="/#sign-in" className="flex gap-2 items-center">
                     <span>Try ZipNote</span>
                     <ArrowRight className="animate-pulse"/>
                     </Link>
