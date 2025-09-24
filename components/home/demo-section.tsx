ecccc420a3e867a5abb698793ba8471d4454bf65
Kartik <kartik3344@gmail.com>
2025-09-24 17:47:10 +0530

    styling and content changes

--- components/home/demo-section.tsx
+++ components/home/demo-section.tsx
@@ -9,7 +9,7 @@ export default function DemoSection(){
         <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
             <div
                 aria-hidden="true"
-                className="pointer-events-none absolute inset-0 -z-10 transform-gpu overflow-hidden blur-3xl">
+                className="pointer-events-none absolute inset-0 z-0 transform-gpu overflow-hidden blur-3xl">
                 <BgGradient />
             </div>
             <div className="flex flex-col items-center text-center space-y-4">
@@ -23,8 +23,8 @@ export default function DemoSection(){
                         whileInView={{y:0, opacity:1}} 
                         transition={{duration: 0.5, delay:0.2}}
                         className="font-bold text-3xl max-w-2xl mx-auto px-4 sm:px-6">
-                        Watch how ZipNote AI{' '}
-                        <span className="bg-linear-to-r from-rose-500 to-rose-700 bg-clip-text text-transparent">Transforms this PDF </span>into an easy-to-read summary!
+                        ZipNote AI{' '}
+                        <span className="bg-linear-to-r from-rose-500 to-rose-700 bg-clip-text text-transparent">Transforms PDFs </span>into quick, readable summaries!
                     </MotionH3>
                 </div>
             </div>
